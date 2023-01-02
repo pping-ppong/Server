1479ba246828c933e53d127db54097608d53494f
yejin <yaezzin@gmail.com>
2023-01-02 16:40:54 +0900

    refactor : 응답에 성공여부 변수 isSuccess 추가

--- src/main/java/com/app/pingpong/global/common/BaseResponse.java
+++ src/main/java/com/app/pingpong/global/common/BaseResponse.java
@@ -1,22 +1,24 @@
 package com.app.pingpong.global.common;
 
-import com.app.pingpong.global.exception.BaseException;
 import com.app.pingpong.global.exception.ErrorCode;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.time.LocalDateTime;
-
 import static com.app.pingpong.global.exception.ErrorCode.SUCCESS;
 
 @Getter
 @AllArgsConstructor
 @Builder
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
 public class BaseResponse<T> {
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
     private final HttpStatus code;
     private final String message;
 
@@ -24,6 +26,7 @@ public class BaseResponse<T> {
     private T result;
 
     public BaseResponse(T result) {
+        this.isSuccess = true;
         this.message = SUCCESS.getMessage();
         this.code = SUCCESS.getHttpStatus();
         this.result = result;
@@ -33,6 +36,7 @@ public class BaseResponse<T> {
         return ResponseEntity
                 .status(errorCode.getHttpStatus())
                 .body(BaseResponse.builder()
+                        .isSuccess(false)
                         .code(errorCode.getHttpStatus())
                         .message(errorCode.getMessage())
                         .build()
