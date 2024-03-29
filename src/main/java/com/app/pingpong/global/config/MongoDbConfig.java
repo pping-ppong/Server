65a4f1178f2facf3e546d968554228b5ce3ecd09
yaezzin <yaezzin@gmail.com>
2024-03-29 15:24:42 +0900

    feat : mongo createdDate

--- src/main/java/com/app/pingpong/global/config/MongoDbConfig.java
+++ src/main/java/com/app/pingpong/global/config/MongoDbConfig.java
@@ -7,9 +7,11 @@ import com.mongodb.client.MongoClients;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoAuditing
 @Configuration
 @EntityScan(basePackages = "com.app.pingpong.domain")
 @EnableMongoRepositories(basePackages = "com.app.pingpong.domain.notification")
